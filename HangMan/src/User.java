fe76f8c68befd2dff5ca143a8ff48eec540abb92
karabash <whyewre@gmail.com>
2016-10-13 17:26:35 +0200

    Revert "Revert "HangTheMan""
    This reverts commit d7bdb58a739204ae803a3bf7631c27726676f752.

---
+++ HangMan/src/User.java
@@ -0,0 +1,24 @@
+
+
+public class User {
+    public String userName;
+    private char letter;
+
+	public String getUserName() {
+		return this.userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	
+	public char getALetter() {
+		return this.letter;
+	}
+
+	public void setALetter(char letter) {
+		this.letter = letter;
+	}
+	
+	
+}
